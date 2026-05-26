c47ef4d4d87e1d82867bbe0ff87594033f7c102e
rogeriodesaf@gmail.com <rogeriodesaf@gmail.com>
2026-05-26 10:01:23 -0300

    Métodos para verificar o progresso do aluno

--- src/main/java/org/rogeriodesaf/progresso/repository/ProgressoRepository.java
+++ src/main/java/org/rogeriodesaf/progresso/repository/ProgressoRepository.java
@@ -6,4 +6,15 @@ import org.rogeriodesaf.progresso.entity.Progresso;
 
 @ApplicationScoped
 public class ProgressoRepository implements PanacheRepository<Progresso> {
+
+    public Progresso buscaPorUsuarioEAula(Long usuarioId, Long aulaId) {
+        return find(
+                "usuarioId = ?1 and aulaId = ?2", usuarioId, aulaId)
+            .firstResult();
+    }
+
+    public Long contarAulasConcluidasPorUsuarioECurso(Long usuarioId, Long cursoId) {
+        return count(
+                "usuarioId = ?1 and cursoId = ?2", usuarioId, cursoId);
+    }
 }
